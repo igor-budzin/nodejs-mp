3f7ed192ce3a3c7a59ef4b1841de44de956f1e7f
Ігор Будзінський <igor.budzin95@gmail.com>
2023-09-08 11:55:25 +0300

    Fix repl

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+const repl = require('repl');
+
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 
-console.log(getRandomNumber(1, 10));
-
-module.exports = getRandomNumber;
+repl.start().context.getRandomNumber = getRandomNumber;
